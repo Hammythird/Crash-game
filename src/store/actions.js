3fc17f1f38f731ccbdc0fcbfc7565b8100977bc2
Hammy <118691667+Hammythird@users.noreply.github.com>
2023-03-30 07:42:40 -0700

    commit

---
+++ src/store/actions.js
@@ -0,0 +1,11 @@
+//BetValueReducer actions
+export const CLEAR_BET_VALUE = 100;
+export const ADD_MAX_BET_VALUE = 1000000;
+export const UPDATE_BET_VALUE = 100;
+
+//balanceReducer
+export const SET_BALANCE = 1000;
+
+//avatarReduce
+
+export const SET_AVATAR = "SET_AVATAR";
